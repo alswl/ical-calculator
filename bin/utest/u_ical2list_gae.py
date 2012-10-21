9b5585ffdbb6eb0728c97f688ad24100b065ff20
one.annum@gmail.com <one.annum@gmail.com@e1cb2c10-74b4-a52f-94e4-9c71d713a321>
2012-10-21 14:39:42 +0000

    added #@UnresolvedImport
    git-svn-id: http://ical-calculator.googlecode.com/svn/trunk@29 e1cb2c10-74b4-a52f-94e4-9c71d713a321

--- bin/utest/u_ical2list_gae.py
+++ bin/utest/u_ical2list_gae.py
@@ -1,15 +1,18 @@
 '''
 Created on 20 Oct 2012
 
-@author: matt_2
+@author: Oberron
 '''
-from test_vect import testvectors,testvector_path
+
+from test_vect import testvectors,testvector_path #@UnresolvedImport
 import urllib
+from datetime import datetime
 import filecmp, sys
+import json
 
 def gae_ical2list():
     tmp = "../../out/tmp.txt"
-    for cal in testvectors[5:6]:
+    for cal in testvectors:
         [file,start,end,reference] = cal
         wFreq = True
 #        print "file is:\t",file,"start is:",datetime.datetime.strftime(datetime.datetime.strptime(start,"%Y%m%d"),"%Y%m%d-%a"),"end is:",end
@@ -18,21 +21,23 @@ def gae_ical2list():
         params = {}
         params['start'] = start
         params['end'] = end
-        params['content'] = src
+        params['snippet'] = src
         params['url'] = ""
-        params['file'] = ""
-        
+        params['file'] = ""        
         params = urllib.urlencode(params)
+#        f = urllib.urlopen("http://localhost:8085/load", params)
         f = urllib.urlopen("http://ical2list.appspot.com/load", params)
         res=f.read()
+#        print res
+        jres = json.loads(res)
         tmpf = open(tmp,'w')
         line =""
-        for reso in res:
-            print res[0], res[1]
-#                    print "reso",reso,start,end
-#            if reso<=end and reso>=start:
-#                line += "{datetime-start: "+reso.strftime("%Y%m%d")+", summary: "+str(event[3])+", uid: "+event[4]+"}\n"
-#                        print "line is",line.replace("\n","")
+        for reso in jres:
+            dstart = datetime.strptime(reso[0][0:10],"%Y-%m-%d")
+            sstart = dstart.strftime("%Y%m%d")
+            summary = reso[1]
+            uid=reso[2]
+            line += "{datetime-start: "+sstart+", summary: "+summary+", uid: "+uid+"}\n"
         tmpf.write(line)
         tmpf.close()
         if filecmp.cmp(tmp,testvector_path+reference,shallow = False):
