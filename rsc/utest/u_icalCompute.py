6f6f29ad7dedd90910a77f8c039bc14f0396704e
one.annum@gmail.com <one.annum@gmail.com@e1cb2c10-74b4-a52f-94e4-9c71d713a321>
2013-01-02 22:54:28 +0000

    1st upload
    git-svn-id: http://ical-calculator.googlecode.com/svn/trunk@73 e1cb2c10-74b4-a52f-94e4-9c71d713a321

---
+++ rsc/utest/u_icalCompute.py
@@ -0,0 +1,55 @@
+'''
+Created on Jan 2, 2013
+
+@author: Oberron
+'''
+import unittest
+import filecmp
+import sys,datetime
+sys.path.append("../../src/") #to overide previous installs
+import ical #@UnresolvedImport
+from test_vect import rrule_vects, testvector_path #@UnresolvedImport
+
+
+class TestIcalCompute(unittest.TestCase):
+    def setUp(self):
+        self.mycal = ical.ics()
+        pass
+
+
+    def tearDown(self):
+        pass
+
+    def test_RRULECompute(self):
+#        print "entering RRULE Compute"
+        vect_index = 0
+        for vect in rrule_vects[0:]:
+            vect_index +=1
+#            print vect
+            [locfile,start,end,reference] = vect
+            #print "file is:\t",file
+            mycal = ical.ics(start,end)
+            mycal.debug(False,"../../out/log.txt")
+            mycal.local_load(testvector_path+locfile)
+            mycal.parse_loaded()
+            mycal.flatten()
+            dates = sorted(mycal.flat_events)
+            tmp = "../../out/tmp.txt"
+            res = open(tmp,'w')
+            for event in  dates:
+                [date, info,uid] =event
+                line = "{datetime-start: "+date.strftime("%Y%m%d")+", summary: "+info+", uid: "+uid+"}\n"
+                res.write(line)
+                #print line
+            res.close()
+            assert filecmp.cmp(tmp,testvector_path+reference,shallow = False)
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestIcalCompute))
+    return suite
+
+if __name__ == "__main__":
+    unittest.main()
+    
