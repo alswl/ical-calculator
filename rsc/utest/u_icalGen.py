74b3567c5a40c00602aec15df06b920c7b5b6dd0
one.annum@gmail.com <one.annum@gmail.com@e1cb2c10-74b4-a52f-94e4-9c71d713a321>
2013-01-02 22:55:00 +0000

    1st upload
    git-svn-id: http://ical-calculator.googlecode.com/svn/trunk@74 e1cb2c10-74b4-a52f-94e4-9c71d713a321

---
+++ rsc/utest/u_icalGen.py
@@ -0,0 +1,42 @@
+'''
+Created on Jan 2, 2013
+
+@author: Oberron
+'''
+import unittest
+import filecmp
+import sys,datetime
+sys.path.append("../../src/") #to overide previous installs
+import ical #@UnresolvedImport
+from test_vect import rrule_vects, testvector_path #@UnresolvedImport
+
+
+class TestIcalGen(unittest.TestCase):
+    def setUp(self):
+        self.mycal = ical.ics()
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_RRULEgen(self):
+#        print "testing RRULE gen"
+        self.mycal.debug(True,LogPath="../../out/log.txt",debug_level=0)
+        cal = rrule_vects[13]
+#        [locfile,start,end,reference] = ["12.ics","20110101","20121231","12OL.txt"]
+        [locfile,start,end,reference] = cal
+        
+        self.mycal.local_load(testvector_path + locfile)
+        self.mycal.parse_loaded()
+        sRRULE = self.mycal.GenRRULEstr(self.mycal.events[0][2])
+        assert sRRULE == "RRULE:FREQ=DAILY;INTERVAL=1"
+#        print "\t \t end test on RRULEgen"
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestIcalGen))
+    return suite
+
+if __name__ == "__main__":
+    unittest.main()
+    
